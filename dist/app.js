6374b46b9166e2aa1d431f3a35ffc13d08977111
SpearJin <ckdwls2525@gmail.com>
2022-04-30 14:50:54 +0900

    5. Vedio 컴포넌트

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import { ImageCompoent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
 class App {
     constructor(appRoot) {
@@ -8,6 +9,8 @@ class App {
         this.page.attachTo(appRoot);
         const image = new ImageCompoent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
+        const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=lvNOcspBdPU');
+        video.attachTo(appRoot, 'beforeend');
         const note = new NoteComponent('Note Title', 'Note Body');
         note.attachTo(appRoot, 'beforeend');
         const todo = new TodoComponent('Todo Title', 'Todo Item');
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageCompoent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -14,6 +15,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://www.youtube.com/watch?v=lvNOcspBdPU'
+    );
+    video.attachTo(appRoot, 'beforeend');
 
     const note = new NoteComponent('Note Title', 'Note Body');
     note.attachTo(appRoot, 'beforeend');
