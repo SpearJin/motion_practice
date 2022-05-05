cc51b8c950a16baef42b5e37cf141892d34ac249
SpearJin <ckdwls2525@gmail.com>
2022-05-05 16:20:49 +0900

    6. PageItem 클래스

--- src/components/component.ts
+++ src/components/component.ts
@@ -4,7 +4,6 @@ export interface Component {
 
 export class BaseComponent<T extends HTMLElement> implements Component {
   protected readonly element: T;
-
   constructor(htmlString: string) {
     const template = document.createElement('template');
     template.innerHTML = htmlString;
