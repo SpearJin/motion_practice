fea5cc4a796fcbc90cb19816feb3e13f5d570b8d
SpearJin <ckdwls2525@gmail.com>
2022-05-06 13:55:47 +0900

    9. 텅빈 다이얼로그 만들기

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,7 @@ import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent, PageItemComponent, } from './components/page/page.js';
+import { InputDialog } from './components/dialog/dialog.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent(PageItemComponent);
@@ -15,6 +16,19 @@ class App {
         this.page.addChild(note);
         const todo = new TodoComponent('Todo Title', 'Todo Item');
         this.page.addChild(todo);
+        const imageBtn = document.querySelector('#new-image');
+        imageBtn.onclick = () => {
+            imageBtn.addEventListener('click', () => {
+                const dialog = new InputDialog();
+                dialog.setOnCloseListenr(() => {
+                    dialog.removeFrom(document.body);
+                });
+                dialog.setOnSubmitListenr(() => {
+                    dialog.removeFrom(document.body);
+                });
+                dialog.attachTo(document.body);
+            });
+        };
     }
 }
 new App(document.querySelector('.document'));
